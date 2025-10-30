62d2e989706811669f61ea800cc87b4daac7f649
oriseg <ori.segev08@gmail.com>
2025-10-30 17:08:51 +0200

    colors

--- blackjack/Views/JoinPopup.xaml.cs
+++ blackjack/Views/JoinPopup.xaml.cs
@@ -7,5 +7,7 @@ public partial class JoinPopup : Popup
 	public JoinPopup()
 	{
 		InitializeComponent();
-	}
+        //Popup.Color controls the background color behind your popup’s content
+        this.Color = Colors.Transparent;
+    }
 }
